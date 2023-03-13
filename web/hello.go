ae7d4239218ec9e1386fa04f5fceda611cbd990e
Giulio Scutari <giulio@20tab.com>
2023-03-13 17:19:22 +0100

    finish Hello World with TDD

--- web/hello.go
+++ web/hello.go
@@ -2,12 +2,32 @@ package main
 
 import "fmt"
 
-func Hello(name string) string{
-	return "Hello, " + name + "!"
+const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return GetGreetingPrefix(language, name)
 }
 
+func GetGreetingPrefix(language string, name string) string {
+	prefix := englishHelloPrefix
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+
+	case french:
+		prefix = frenchHelloPrefix
+	}
+	return prefix + name + "!"
+}
 
 func main() {
-	fmt.Println(Hello("Olly"))
+	fmt.Println(Hello("Olly", ""))
 
 }
